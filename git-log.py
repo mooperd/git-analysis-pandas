88f6358aaf87ceba86faaed0526eded74140fd51
Andrew Holway <andrew.holway@gmail.com>
2020-11-29 16:47:33 +0100

    updates

--- git-log.py
+++ git-log.py
@@ -3,7 +3,10 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 dataframe = pd.read_csv("git-log-2020.csv")
 
-dataframe['date'] = pd.to_datetime(dataframe['date'], errors="coerce")
+dataframe['date'] = pd.to_datetime(dataframe['date'])
+grouped_dataframe_count = dataframe.groupby([pd.Grouper(key='date', freq='M'), "email"])[["subject"]].count()
 
-grouped_dataframe = dataframe.groupby([pd.Grouper(key='date', freq='M'), "email"])[["comment"]].count()
-grouped_dataframe.sort_values("comment", ascending=False)
+# Select all the users that have contributed more than 20 times.
+print(grouped_dataframe[grouped_dataframe['subject'] > 20])
+
+
